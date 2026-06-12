01fd7e2cf62ae03e2274073cc0c4fad3a7b411bd
Nadirrashidi <111681837+Nadirrashidi@users.noreply.github.com>
2026-06-12 18:21:14 +0530

    Update silver_dim_customer.sql

--- models/silver/silver_dim_customer.sql
+++ models/silver/silver_dim_customer.sql
@@ -10,14 +10,15 @@ with bronze_dim_customer as (
 )
 
 select 
-  {{ dbt_utils.generate_surrogate_key(['customer_id', 'city']) }} as customer_hash_key,
+  -- 💡 CHANGED: 'customer_id' ki jagah 'customer_code' kiya, aur 'city' hata diya kyunki woh table mein nahi hai
+  {{ dbt_utils.generate_surrogate_key(['customer_code']) }} as customer_hash_key,
   cast(customer_sk as int) as customer_sk,
   upper(first_name) as first_name,
   upper(last_name) as last_name,
   coalesce(email, 'NOT AVAILABLE') as email,
-   loyalty_tier,
-   signup_date,
-   year(signup_date) as signup_year,
-   month(signup_date) as signup_month
+  loyalty_tier,
+  signup_date,
+  year(signup_date) as signup_year,
+  month(signup_date) as signup_month
 from bronze_dim_customer
 where customer_sk is not null
